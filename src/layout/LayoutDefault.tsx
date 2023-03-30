5e5830cdf9bfcc3b313a29f06da2b05503868a03
ThanhTrong <tronglagi111222@gmail.com>
2023-03-31 00:17:55 +0700

    Change version of project and set up LayoutDefault

--- src/layout/LayoutDefault.tsx
+++ src/layout/LayoutDefault.tsx
@@ -10,22 +10,7 @@ const LayoutDefault = () => {
             </div>
             <div className='flex flex-col w-full'>
                 <Navbar />
-                {/* <Outlet /> */}
-                <div className='flex h-screen flex-col m-auto'>
-                    <div className='flex justify-between'>
-                        <ul className='flex'></ul>
-                        <div className='p-4 mr-4'>
-                            <p className='text-white font-semibold text-2xl cursor-pointer'>
-                                Sign-in
-                            </p>
-                        </div>
-                    </div>
-                    <div className='flex flex-col'>
-                        <code className='m-auto text-white text-7xl font-bold'>
-                            console.log('abc'); &ensp; thanh trong
-                        </code>
-                    </div>
-                </div>
+                <Outlet />
             </div>
         </div>
     );
