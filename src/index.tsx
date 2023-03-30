9ca31032a97aee768b1de59c1c77d882de01dff2
ThanhTrong <tronglagi111222@gmail.com>
2023-03-31 00:55:00 +0700

    Create HomePage, route HomePage with LayoutDefault

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,15 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import DeclareRouter from './route';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <div className='flex bg-gradient-to-r from-orange-400 via-red-500 to-pink-500 bg-repeat h-screen flex-col'>
         <React.StrictMode>
             <Router>
-                <App />
+                {/* <App /> */}
+                <DeclareRouter />
                 <ToastContainer />
             </Router>
         </React.StrictMode>
--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import LayoutDefault from '~/layout/LayoutDefault';
+import Home from '~/screens/home/Home';
 import PrivateRoute from './privateRoute/PrivateRoute';
 
 function DeclareRouter() {
@@ -9,21 +11,23 @@ function DeclareRouter() {
                 <Route path='/' element={<Navigate to={'/admin'} />} />
                 <Route path='/login' />
                 <Route path='/admin' element={<PrivateRoute />}>
-                    <Route path='' />
-                    <Route path='users' />
-                    <Route path='user'>
-                        <Route path=':userId' />
-                        <Route path='new' />
-                    </Route>
-                    <Route path='products' />
-                    <Route path='product'>
-                        <Route path=':productId' />
-                        <Route path='new' />
-                    </Route>
-                    <Route path='sales' />
-                    <Route path='sale'>
-                        <Route path=':saleId' />
-                        <Route path='new' />
+                    <Route element={<LayoutDefault />}>
+                        <Route path='' element={<Home />} />
+                        <Route path='users' />
+                        <Route path='user'>
+                            <Route path=':userId' />
+                            <Route path='new' />
+                        </Route>
+                        <Route path='products' />
+                        <Route path='product'>
+                            <Route path=':productId' />
+                            <Route path='new' />
+                        </Route>
+                        <Route path='sales' />
+                        <Route path='sale'>
+                            <Route path=':saleId' />
+                            <Route path='new' />
+                        </Route>
                     </Route>
                 </Route>
             </Routes>
