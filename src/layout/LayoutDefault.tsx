05762900d2b6966c0fcf5b169fae28f3b92fd69a
ThanhTrong <trongnt@itsj-group.com.vn>
2023-04-03 00:10:25 +0700

    Config paths

--- src/layout/LayoutDefault.tsx
+++ src/layout/LayoutDefault.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { Navbar, Sidebar } from '../modules';
+import { Navbar, Sidebar } from '~/modules';
 
 const LayoutDefault = () => {
     return (
