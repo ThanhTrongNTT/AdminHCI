8a4c9c105049d8df04aa6483f91d5e07d0e7a26a
ThanhTrong <trongnt@itsj-group.com.vn>
2023-04-03 17:12:47 +0700

    Change Background to html

--- src/index.tsx
+++ src/index.tsx
@@ -9,13 +9,11 @@ import DeclareRouter from './route';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <div className='flex bg-gradient-to-r from-orange-400 via-red-500 to-pink-500 bg-repeat flex-col'>
-        <React.StrictMode>
-            <Router>
-                {/* <App /> */} // Change from App to DeclareRouter
-                <DeclareRouter />
-                <ToastContainer />
-            </Router>
-        </React.StrictMode>
-    </div>,
+    <React.StrictMode>
+        <Router>
+            {/* <App />  // Change from App to DeclareRouter*/}
+            <DeclareRouter />
+            <ToastContainer />
+        </Router>
+    </React.StrictMode>,
 );
