88eeeecc711a0d511d6f468e4d14fd8ee51a9f89
ThanhTrong <trongnt@itsj-group.com.vn>
2023-04-05 14:42:52 +0700

    Create method to call Authentication APIs

---
+++ src/api/auth.api.ts
@@ -0,0 +1,54 @@
+import {
+    ConfirmEmailDTO,
+    ForgotPasswordDTO,
+    LoginDTO,
+    RegisterDTO,
+    ResetPasswordDTO,
+    UpdateAccountDTO,
+    UpdatePasswordDTO,
+} from '~/data/AuthInterface';
+import AxiosClient from './axiosClient/AxiosClient';
+
+const authApi = {
+    register: (data: RegisterDTO) => {
+        const url = 'auth/register';
+        return AxiosClient.post(url, { data });
+    },
+    login: (data: LoginDTO) => {
+        const url = 'auth/log-in';
+        return AxiosClient.post(url, { data });
+    },
+    confirmEmail: (data: ConfirmEmailDTO) => {
+        const url = 'auth/confirm-email';
+        return AxiosClient.post(url, data);
+    },
+    resendToken: () => {
+        const url = 'auth/resend-token';
+        return AxiosClient.post(url);
+    },
+    refreshToken: () => {
+        const url = 'auth/refresh-token';
+        return AxiosClient.get(url);
+    },
+    logout: () => {
+        const url = 'auth/log-out';
+        return AxiosClient.post(url);
+    },
+    updatePassword: (data: UpdatePasswordDTO) => {
+        const url = 'auth/password';
+        return AxiosClient.post(url, data);
+    },
+    updateInformation: (data: UpdateAccountDTO) => {
+        const url = 'auth/update-account';
+        return AxiosClient.post(url, data);
+    },
+    forgotPassword: (data: ForgotPasswordDTO) => {
+        const url = 'auth/forgot-password';
+        return AxiosClient.post(url, data);
+    },
+    resetPassword: (tokenId: string, data: ResetPasswordDTO) => {
+        const url = `auth/reset-password/${tokenId}`;
+        return AxiosClient.post(url, data);
+    },
+};
+export default authApi;
---
+++ src/data/AuthInterface.ts
@@ -0,0 +1,28 @@
+export interface RegisterDTO {
+    email: string;
+    password: string;
+}
+export interface LoginDTO {
+    email: string;
+    password: string;
+}
+export interface ConfirmEmailDTO {
+    token: string;
+}
+export interface UpdatePasswordDTO {
+    newPassword: string;
+    oldPassword: string;
+}
+export interface UpdateAccountDTO {
+    email: string;
+    address: string;
+    fullName: string;
+    phoneNumber: string;
+    gender: string;
+}
+export interface ForgotPasswordDTO {
+    email: string;
+}
+export interface ResetPasswordDTO {
+    password: string;
+}
